19ac27d825631a0ede1e06897fdaa6b1b8b89b4f
la122 <70019096+la122@users.noreply.github.com>
2024-10-03 16:52:35 +0200

    Recursion project done

---
+++ projects/recusion.rb
@@ -0,0 +1,56 @@
+def fibs(n)
+  result = []
+  n.times do |i|
+    result << case i
+              when 0 then 0
+              when 1 then 1
+              when 2 then 1
+              else (result[-1] + result[-2])
+              end
+  end
+  result
+end
+
+def fibs_rec(n)
+  puts 'This was printed recursively'
+  return [] if n < 1
+
+  case n
+  when 1 then [0]
+  when 2 then [0, 1]
+  else
+    result = fibs_rec(n - 1)
+    result << result[-1] + result[-2]
+  end
+end
+
+def merge_sort(array)
+  case array
+  in [] | [_] then array
+  else
+    half = array.length / 2
+    left = merge_sort(array[...half])
+    right = merge_sort(array[half..])
+    merged = []
+    until left.empty? && right.empty?
+      merged << case
+                when right.empty?
+                  left.shift
+                when left.empty?
+                  right.shift
+                when left.first < right.first
+                  left.shift
+                else
+                  right.shift
+                end
+    end
+    merged
+  end
+end
+
+n = 8
+p fibs(n)
+p fibs_rec(n)
+
+p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([105, 79, 100, 110])
