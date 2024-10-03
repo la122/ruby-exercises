49ead3060fc773328b7fa4ec4d43b86d0c08643c
la122 <70019096+la122@users.noreply.github.com>
2024-10-03 17:15:47 +0200

    improved recursive after checking solutions

--- projects/recusion.rb
+++ projects/recusion.rb
@@ -4,7 +4,6 @@ def fibs(n)
     result << case i
               when 0 then 0
               when 1 then 1
-              when 2 then 1
               else (result[-1] + result[-2])
               end
   end
@@ -13,39 +12,32 @@ end
 
 def fibs_rec(n)
   puts 'This was printed recursively'
-  return [] if n < 1
+  return [] if n.zero?
+  return [0] if n == 1
+  return [0, 1] if n == 2
 
-  case n
-  when 1 then [0]
-  when 2 then [0, 1]
-  else
-    result = fibs_rec(n - 1)
-    result << result[-1] + result[-2]
-  end
+  result = fibs_rec(n - 1)
+  result << result[-1] + result[-2]
 end
 
 def merge_sort(array)
-  case array
-  in [] | [_] then array
-  else
-    half = array.length / 2
-    left = merge_sort(array[...half])
-    right = merge_sort(array[half..])
-    merged = []
-    until left.empty? && right.empty?
-      merged << case
-                when right.empty?
-                  left.shift
-                when left.empty?
-                  right.shift
-                when left.first < right.first
-                  left.shift
-                else
-                  right.shift
-                end
-    end
-    merged
+  return array if array.length < 2
+
+  half = array.length / 2
+  left = merge_sort(array[...half])
+  right = merge_sort(array[half..])
+
+  merged = []
+
+  until left.empty? || right.empty?
+    merged << if left.first < right.first
+                left.shift
+              else
+                right.shift
+              end
   end
+
+  merged + left + right
 end
 
 n = 8
